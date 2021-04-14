756452089c9e447977ca046e36890d97daf8c901
Kang0325 <noworry1@naver.com>
2021-04-14 17:28:06 +0900

    210414 수업 끝

---
+++ DotNetNote/Helpers.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helpers.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helpers.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e858c882-05ac-45a7-9c0a-c65d0c5d799f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HelloAspNet/TestDB_Database/dbo/Tables/Users.sql
@@ -0,0 +1,11 @@
+﻿--[User][0][1] 회원관리를 위한 Users 테이블 생성
+
+--[0] Users 테이블 생성
+Create Table dbo.Users
+(
+	UID Int Identity(1, 1) Primary Key Not Null,
+	UserID NVarChar(25) Not Null,
+	[Password] NVarChar(20) Not Null
+    -- 필요한 항목이 있으면, 언제든 추가
+)
+Go
