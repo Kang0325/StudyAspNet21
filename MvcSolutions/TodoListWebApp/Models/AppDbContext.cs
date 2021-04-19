38cdeb6d9e077d9f9a7830877d99044ddfa57cfe
Kang0325 <noworry1@naver.com>
2021-04-19 17:10:57 +0900

    210419 MVC

---
+++ MvcSolutions/TodoListWebApp/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoListWebApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
